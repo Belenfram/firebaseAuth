9f33457b3c24d2b27b893d9ce274b1dd79ec1ddd
Belenfram <135793524+Belenfram@users.noreply.github.com>
2024-01-19 23:41:02 -0600

    v2

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-  /*import {app} from "./firebase.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+//import "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js"; // Cambio en esta línea
 
-  const form = document.forms['formLogin'];
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDGxcIUwJziFEUWREs3crhZZCG0mCvkbro",
+  authDomain: "aweb40-1884f.firebaseapp.com",
+  projectId: "aweb40-1884f",
+  storageBucket: "aweb40-1884f.appspot.com",
+  messagingSenderId: "269676533678",
+  appId: "1:269676533678:web:79e0990fec2339482a1a3f",
+  measurementId: "G-7DKG47JE4N"
+};
 
-  form.addEventListener('submit',handleForSubmit);
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
 
-  function handleForSubmit(event){
-    event.preventDefault();
+  const formLogin = document.querySelector("#formLogin");
 
-    
-
-    //console.log(`Correo: ${email}, Contraseña: ${password}`); 
-    return loginUser(email, password);
-  }*/
-
-  function loginUser(){
-
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    console.log(`Correo: ${email}, Contraseña: ${password}`); 
-
-    //metodo para la autenticación
-    firebase.auth().signInWithEmailAndPassword(email, password)
-     .then((success) => {
-      console.log('Credenciales correctas')
-     })
-     .catch((error) => {
-      console.log(error);
-     });
-  }
+  formLogin.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+  
+    signInWithEmailAndPassword(auth,email,password).then(cred=>{
+      alert('Credenciales Correctas/signIn');
+      console.log('credeciales correctas');
+      //console.log(cred.user)
+    }).catch(e=>{
+      const errorCode = e.code;
+      console.log(errorCode);
+    })
+  
+  });
